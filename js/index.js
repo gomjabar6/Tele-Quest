c80303db14c1b5fe40a105ffd7af7a8e2eda8f26
cluzier <connerluzier@protonmail.com>
2020-03-29 11:42:58 -0400

    secret admin page, raid grid updated

--- js/index.js
+++ js/index.js
@@ -70,10 +70,44 @@ const raids = getJSON(
     } else {
       document.getElementById("raidBoss0").innerHTML = data[0].name;
       document.getElementById("sessionId0").innerHTML = data[0].date;
+      document.getElementById("xpReq0").innerHTML = data[0].xpLevel;
       document.getElementById("raidReward0").innerHTML = data[0].xpReward;
       document.getElementById("raidBoss1").innerHTML = data[1].name;
       document.getElementById("sessionId1").innerHTML = data[1].date;
+      document.getElementById("xpReq1").innerHTML = data[1].xpLevel;
       document.getElementById("raidReward1").innerHTML = data[1].xpReward;
+      document.getElementById("raidBoss2").innerHTML = data[2].name;
+      document.getElementById("sessionId2").innerHTML = data[2].date;
+      document.getElementById("xpReq2").innerHTML = data[2].xpLevel;
+      document.getElementById("raidReward2").innerHTML = data[2].xpReward;
+      document.getElementById("raidBoss3").innerHTML = data[3].name;
+      document.getElementById("sessionId3").innerHTML = data[3].date;
+      document.getElementById("xpReq3").innerHTML = data[3].xpLevel;
+      document.getElementById("raidReward3").innerHTML = data[3].xpReward;
+      document.getElementById("raidBoss4").innerHTML = data[4].name;
+      document.getElementById("sessionId4").innerHTML = data[4].date;
+      document.getElementById("xpReq4").innerHTML = data[4].xpLevel;
+      document.getElementById("raidReward4").innerHTML = data[4].xpReward;
+      document.getElementById("raidBoss5").innerHTML = data[5].name;
+      document.getElementById("sessionId5").innerHTML = data[5].date;
+      document.getElementById("xpReq5").innerHTML = data[5].xpLevel;
+      document.getElementById("raidReward5").innerHTML = data[5].xpReward;
+      document.getElementById("raidBoss6").innerHTML = data[6].name;
+      document.getElementById("sessionId6").innerHTML = data[6].date;
+      document.getElementById("xpReq6").innerHTML = data[6].xpLevel;
+      document.getElementById("raidReward6").innerHTML = data[6].xpReward;
+      document.getElementById("raidBoss7").innerHTML = data[7].name;
+      document.getElementById("sessionId7").innerHTML = data[7].date;
+      document.getElementById("xpReq7").innerHTML = data[7].xpLevel;
+      document.getElementById("raidReward7").innerHTML = data[7].xpReward;
+      document.getElementById("raidBoss8").innerHTML = data[8].name;
+      document.getElementById("sessionId8").innerHTML = data[8].date;
+      document.getElementById("xpReq8").innerHTML = data[8].xpLevel;
+      document.getElementById("raidReward8").innerHTML = data[8].xpReward;
+      document.getElementById("raidBoss9").innerHTML = data[9].name;
+      document.getElementById("sessionId9").innerHTML = data[9].date;
+      document.getElementById("xpReq9").innerHTML = data[9].xpLevel;
+      document.getElementById("raidReward9").innerHTML = data[9].xpReward;
     }
   }
 );
